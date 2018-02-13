5be74aee97f1084346e9373cb6310fa81bebc31d
adam <starksmile@hotmail.com>
2018-02-13 16:27:07 +0800

    MVC first attempt

--- src/main/java/msm_group/masterspringmvc/controller/HelloController.java
+++ src/main/java/msm_group/masterspringmvc/controller/HelloController.java
@@ -1,13 +1,16 @@
 package msm_group.masterspringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello() {
+    public String hello(@RequestParam(value = "name", defaultValue = "AnyUser", required = true) String userName, Model model) {
+        model.addAttribute("message","Hello from the controller, " + userName);
         return "resultPage";
     }
 
