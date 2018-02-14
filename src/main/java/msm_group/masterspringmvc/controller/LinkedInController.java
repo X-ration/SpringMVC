712cc7defee777afae9dfbe07eec1a1c37609039
adam <starksmile@hotmail.com>
2018-02-14 14:13:27 +0800

    Add simple error effect.

--- src/main/java/msm_group/masterspringmvc/controller/LinkedInController.java
+++ src/main/java/msm_group/masterspringmvc/controller/LinkedInController.java
@@ -4,7 +4,12 @@ import msm_group.masterspringmvc.linkedin.LinkedIn;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LinkedInController {
@@ -26,5 +31,21 @@ public class LinkedInController {
 
         return "resultPage";
     }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String keyword = request.getParameter("keyword");
+        if(keyword.equals("")) {
+            redirectAttributes.addFlashAttribute("error","Empty word not permitted! Please try another word.");
+            return "redirect:/";
+        }
+        if(linkedIn.searchJobs(keyword).size() == 0) {
+            redirectAttributes.addFlashAttribute("error", "No result found! Please try another word.");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("keyword", keyword);
+        return "redirect:result";
+    }
+
 
 }
