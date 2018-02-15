f3bee55ccdbcd377b0a9fbbfb4439aa5fe1380d1
adam <starksmile@hotmail.com>
2018-02-15 13:18:08 +0800

    Add tastes fieldset.

--- src/main/java/msm_group/masterspringmvc/profile/ProfileController.java
+++ src/main/java/msm_group/masterspringmvc/profile/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -18,15 +19,27 @@ public class ProfileController {
         return "profile/profilePage";
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @RequestMapping(value = "/profile", params = {"save"}, method = RequestMethod.POST)
     public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println("[POST]has error");
             return "profile/profilePage";
         }
-
         System.out.println("[POST]save ok" + profileForm);
         return "redirect:/profile";
+    }
+
+    @RequestMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", params = {"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest httpServletRequest) {
+        Integer rowId = Integer.valueOf(httpServletRequest.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
     }
 
     @ModelAttribute("dateFormat")  //暴露dateFormat属性给Web页面
