b44910c1991b642b54b6223fc09c8fc5755678da
adam <starksmile@hotmail.com>
2018-02-17 12:06:23 +0800

    Add a simple api rest search controller(@RestController).

---
+++ src/main/java/msm_group/masterspringmvc/search/api/SearchApiController.java
@@ -0,0 +1,26 @@
+package msm_group.masterspringmvc.search.api;
+
+import msm_group.masterspringmvc.linkedin.Job;
+import msm_group.masterspringmvc.search.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController  //返回类型序列化为合适的格式，默认为JSON。
+@RequestMapping("/api/search")
+public class SearchApiController {
+
+    private SearchService searchService;
+
+    @Autowired
+    public SearchApiController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @RequestMapping(value = "/{searchType}", method = RequestMethod.GET)
+    public List<Job> search(@PathVariable String searchType, @MatrixVariable List<String> keywords) {
+        return searchService.search(searchType, keywords);
+    }
+
+}
