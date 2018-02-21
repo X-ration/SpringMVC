99623c80b80e6b25920df988a8628df3891caf5c
adam <starksmile@hotmail.com>
2018-02-21 15:58:38 +0800

    Use stub to test SearchController.

---
+++ src/test/java/msm_group/masterspringmvc/search/SearchControllerStubTest.java
@@ -0,0 +1,48 @@
+package msm_group.masterspringmvc.search;
+
+import msm_group.masterspringmvc.MasterSpringMvcApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {
+        MasterSpringMvcApplication.class,
+        StubLinkedInSearchConfig.class
+})
+public class SearchControllerStubTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void should_search() throws Exception {
+        this.mockMvc.perform(get("/search/mixed;keywords=spring"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("resultPage"))
+                .andExpect(model().attribute("jobs_searched",hasSize(2)))
+                .andExpect(model().attribute("jobs_searched",
+                        hasItems(hasProperty("title",is("jobTitle")),
+                                hasProperty("title",is("secondJob"))
+                        ))
+                );
+    }
+
+}
---
+++ src/test/java/msm_group/masterspringmvc/search/StubLinkedInSearchConfig.java
@@ -0,0 +1,25 @@
+package msm_group.masterspringmvc.search;
+
+import msm_group.masterspringmvc.linkedin.LightJobBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Arrays;
+
+/**
+ * @author adam
+ * Stub测试中LinkedInSearch接口的替代实现。
+ */
+@Configuration
+public class StubLinkedInSearchConfig {
+
+    @Primary @Bean
+    public LinkedInSearch linkedInSearch() {
+        return ((searchType, keywords) -> Arrays.asList(
+                new LightJobBuilder().title("jobTitle").build(),
+                new LightJobBuilder().title("secondJob").build())
+        );
+    }
+
+}
